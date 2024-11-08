0a2f4e160cb74c9c815cfab9c3631ab8cbb57996
Leif Andersen <leif@leifandersen.net>
2024-11-08 13:34:36 -0500

    Switching to hono because:
    It provides MUCH nicer server side rendering.
    
    (Client side rendering will still probably done with React.)
    
    Avoiding something like next because while its more popular, its a
    framwork with stricter requirements on how we set up the project, that
    we may or may not want in the long run.

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import { port, secret } from './env.ts';
 import { createClient } from './auth/client.ts';
 import { type NodeOAuthClient } from '@atproto/oauth-client-node';
 import { Agent } from '@atproto/api';
-import Fastify, { type FastifyReply, type FastifyRequest } from 'fastify';
-import formbody from '@fastify/formbody';
-import multipart from '@fastify/multipart';
 import { getIronSession } from 'iron-session';
 import assert from 'node:assert';
-import type { IncomingMessage, ServerResponse } from 'node:http'
-
+import { Hono, type Context } from 'hono';
+import { logger } from 'hono/logger';
+import { serve } from '@hono/node-server';
 
 type Session = { did: string }
 
@@ -34,8 +32,8 @@ const run = async () => {
 
   let oauthClient: NodeOAuthClient = await createClient();
 
-  const getSessionAgent = async (req: FastifyRequest, res: FastifyReply) => {
-    const session = await getIronSession<Session>(req.raw, res.raw, {
+  const getSessionAgent = async (c: Context) => {
+    const session = await getIronSession<Session>(c.req.raw, c.res, {
       cookieName: "sid",
       password: secret
     });
@@ -49,61 +47,59 @@ const run = async () => {
     }
   }
 
-  const server = Fastify({ logger: true });
-  server.register(formbody);
-  server.register(multipart);
+  const server = new Hono();
+  //server.use(logger());
 
-  server.get("/", async (req, res) => {
-    const agent = await getSessionAgent(req, res);
+  server.get("/", async (c) => {
+    const agent = await getSessionAgent(c);
 
-    if (agent) res.send("LOGGED IN!");
-    else res.type("html").send(index());
+    if (agent) return c.text("LOGGED IN!");
+    else return c.html(index());
   });
 
-  server.post("/login", async (req, res) => {
-    let data = await req.body as { bskyid: string };
-    const handle: string = data?.bskyid?.toString() ?? "";
+  server.post("/login", async (c) => {
+    let data = await c.req.formData();
+    const handle: string = data.get('bskyid')?.toString() ?? "";
     const url = await oauthClient.authorize(handle, {
       scope: "atproto transition:generic"
-    })
-    res.redirect(url.toString());
+    });
+    return c.redirect(url.toString());
   });
 
-  server.post("/logout", async (req, res) => {
-    const session = await getIronSession<Session>(req.raw, res.raw, {
+  server.post("/logout", async (c) => {
+    const session = await getIronSession<Session>(c.req.raw, c.res, {
       cookieName: "sid",
       password: secret
     });
     await session.destroy();
-    return res.redirect("/");
+    return c.redirect("/");
   })
 
-  server.get("/client-metadata.json", async (req, res) => { res.send(oauthClient.clientMetadata) })
+  server.get("/client-metadata.json", async (c) => {
+    return c.json(oauthClient.clientMetadata) 
+  });
 
-  server.get("/oauth/callback", async (req, res) => {
-    const params = new URLSearchParams(req.originalUrl.split("?")[1])
+  server.get("/oauth/callback", async (c) => {
+    const params = new URLSearchParams(c.req.url.split("?")[1])
     try {
       const { session } = await oauthClient.callback(params);
-      const clientSession = await getIronSession<Session>(req.raw, res.raw, {
+      const clientSession = await getIronSession<Session>(c.req.raw, c.res, {
         cookieName: "sid",
         password: secret
       });
       assert(!clientSession.did, "session already exists");
       clientSession.did = session.did;
       await clientSession.save();
-      return res.redirect("/");
+      return c.redirect("/");
     } catch (err) {
-      server.log.error(err);
-      return res.redirect("/?error");
+      //server.log.error(err);
+      console.error(err);
+      return c.redirect("/?error");
     }
   });
 
-  try {
-    await server.listen({ port, host: '0.0.0.0' })
-  } catch (err) {
-    server.log.error(err);
-    process.exit();
-  }
+  serve({fetch: server.fetch, port});
+
 }
 
 run()
